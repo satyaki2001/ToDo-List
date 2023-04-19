fdccda151714025196b7b03e1d77639d687d2691
satyaki2001 <satyakisinha0@gmail.com>
2023-04-19 12:00:23 +0530

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,41 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+const token = localStorage.getItem('access_token');
 const Header = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get("https://servicesv1.sangria.tech/user/getLoggedInUser", {
-          headers: { Authorization: localStorage.getItem('access_token') },
-        });
-        setUser(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchUser();
-  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('access_token');
     navigate('/');
   };
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if(!token) return;
+      try {
+        const response = await axios.get("https://servicesv1.sangria.tech/user/getLoggedInUser",{
+          headers: {
+            Authorization: token,
+        },
+        });
+        setUsername(response.data.username);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <header className='Header'>
       <h1 className='heading'>
         <i className='fa-regular fa-pen-to-square'></i> To Do List
       </h1>
-      {user && <p>hello{user.name}</p>}
+      <div className='user-info'>
+      <span>{username}</span>
       <i
         onClick={handleLogout}
         title='LogOut?'
         className='fa-solid fa-right-from-bracket user-access'
         style={{ color: '#ebebeb' }}
       ></i>
+      </div>
     </header>
   );
 };
